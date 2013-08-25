4a86d677cbb1357e9beaf817d7b54e65f8f8acac
andresme <andres.me@gmail.com>
2013-08-25 17:51:43 -0600

    Added Desktop version

---
+++ Save_The_Stars-Desktop/src/ludum/dare/anmorales/Main.java
@@ -0,0 +1,16 @@
+package ludum.dare.anmorales;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Save the Stars!";
+		cfg.useGL20 = true;
+		cfg.width = 640;
+		cfg.height = 360;
+		
+		new LwjglApplication(new SaveTheStars(), cfg);
+	}
+}
